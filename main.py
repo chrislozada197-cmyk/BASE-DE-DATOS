b178612b02759a9f4741348e8f416a4dbac8422f
chrislozada197-cmyk <chrislozada197@gmail.com>
2026-06-27 11:14:26 -0500

    Refactor backup route and remove email functionality

--- main.py
+++ main.py
@@ -1,49 +1,12 @@
-from flask import Flask, jsonify
-import smtplib
-from email.message import EmailMessage
-import json
+from flask import Flask
 
 app = Flask(__name__)
 
-EMAIL = "chrislozada197@gmail.com"
-APP_PASSWORD = "wnut jysi afxm eeee"
-
-
 @app.route("/")
 def home():
-    return "ARCHIVO CORRECTO CON BACKUP"
-
-
+    return "FUNCIONA ROOT"
 
 @app.route("/backup")
 def backup():
-    try:
-        # datos simples
-        datos = {"mensaje": "backup funcionando"}
-
-        # guardar archivo
-        with open("backup.json", "w") as f:
-            json.dump(datos, f)
-
-        # email
-        msg = EmailMessage()
-        msg["Subject"] = "Backup Flask"
-        msg["From"] = EMAIL
-        msg["To"] = EMAIL
-
-        with open("backup.json", "rb") as f:
-            msg.add_attachment(
-                f.read(),
-                maintype="application",
-                subtype="json",
-                filename="backup.json"
-            )
-
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-            smtp.login(EMAIL, APP_PASSWORD)
-            smtp.send_message(msg)
-
-        return "BACKUP ENVIADO OK"
-
-    except Exception as e:
-        return "ERROR: " + str(e)
+    return "FUNCIONA BACKUP"
+``
